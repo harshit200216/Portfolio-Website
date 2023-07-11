63d3c26b7d933eb983b600912778d3f8c73b23f6
Harshit Ram Tripathi <harshitramtripathi2000@gmail.com>
2023-07-11 19:04:42 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ navList=nav.querySelectorAll("li"),
 totalNavList=navList.length,
 allSection=document.querySelectorAll(".section"),
 totalSection=allSection.length;
-for(let i=0;i<totalNavList;i++){
+for(let i=0;i<totalNavList;i++){ 
   const a=navList[i].querySelector("a");
   a.addEventListener("click",function(){
     for(let i=0;i<totalSection;i++){
